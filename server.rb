bba2d1c74c1e687b484e6f9631ec2c5c3ddc406a
innlouvate <l.j.franklin85@googlemail.com>
2017-07-02 22:19:02 +0100

    trivia or math questoins and super basic error handling

--- server.rb
+++ server.rb
@@ -3,17 +3,32 @@ require 'json'
 require 'net/http'
 
 post '/' do
-  number = JSON.parse(request.body.read)["request"]["intent"]["slots"]["Number"]["value"]
-  number_facts_uri = URI("http://numbersapi.com/#{number}")
+  parsed_request = JSON.parse(request.body.read)
+  number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
+  fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
+
+  number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
   number_fact = Net::HTTP.get(number_facts_uri)
 
-  {
-    version: "1.0",
-    response: {
-      outputSpeech: {
-          type: "PlainText",
-          text: number_fact
-        }
-    }
-  }.to_json
+  if (!number)
+    {
+      version: "1.0",
+      response: {
+        outputSpeech: {
+            type: "PlainText",
+            text: "Please format your question as, tell me about a math or trivia fact about a number"
+          }
+      }
+    }.to_json
+  else
+    {
+      version: "1.0",
+      response: {
+        outputSpeech: {
+            type: "PlainText",
+            text: number_fact
+          }
+      }
+    }.to_json
+  end
 end
